3d8e63a3c3a69c19c864a9eb8c6cb3b82dadc5d7
Taejoon Kwon <taejoon.kwon@mail.utexas.edu>
2011-09-26 15:19:47 -0500

    Add scripts to convert SAM to target_freq.

--- sam/sam2target_freq.sh
+++ sam/sam2target_freq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for SAM in $(ls *.sam_slim)
+for SAM in $(ls *.sam)
 do
-  TARGET_FREQ=${SAM%.sam_slim}".target_freq"
+  TARGET_FREQ=${SAM%.sam}".target_freq"
   echo "$SAM --> $TARGET_FREQ"
   gawk -F"\t" '{print $3"\t"$4"\t"$2"\t"$6}' $SAM | sort | uniq -c | sort -k 2,2 -k 3,3n > $TARGET_FREQ
 done

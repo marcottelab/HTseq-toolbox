cfb30fdc30bf6a7021fad179a68230ee1657dff9
Taejoon Kwon <linusben@login1.longhorn>
2011-10-31 16:28:46 -0500

    Revised script to make table from fastq.

--- fastq/fastq-to-tbl.py
+++ fastq/fastq-to-tbl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
+import re
 import gzip
 
 filename_fq = sys.argv[1]
@@ -9,13 +10,16 @@ f_fq = open(filename_fq,'r')
 if( filename_fq.endswith('gz') ):
     f_fq = gzip.open(filename_fq,'rb')
 
-filename_out = '%s_tbl'%(filename_fq.replace('.gz',''))
-f_out = open(filename_out,'w')
-f_out.write('#ID\tseq\tqseq\tQmean\tQmedian\tQmin\n')
+#filename_out = '%s_tbl'%(filename_fq.replace('.gz',''))
+#f_out = open(filename_out,'w')
+#f_out.write('#ID\tseq\tqseq\tQmean\tQmedian\tQmin\n')
+sys.stdout.write('#ID\tseq\tqseq\tQmean\tQmedian\tQmin\n')
 sys.stderr.write('Write %s\n'%filename_out)
 for h in f_fq:
     if( h.startswith('@') ):
-        h = h.strip()
+        h = h.split()[0]
+        h = re.sub('/[12]$','',h.strip())
+        h = re.sub('/_[FR][35]$','',h.strip())
         nseq = f_fq.next().strip()
         qh = f_fq.next()
         qseq = f_fq.next().strip()
@@ -23,6 +27,6 @@ for h in f_fq:
         q_mean = int(float(sum(q_list))/len(q_list))
         q_median = sorted(q_list)[int(len(q_list)*0.5)]
         q_min = min(q_list)
-        f_out.write("%s\t%s\t%s\t%d\t%d\t%d\n"%(h,nseq,qseq,q_mean,q_median,q_min))
+        sys.stdout.write("%s\t%s\t%s\t%d\t%d\t%d\n"%(h,nseq,qseq,q_mean,q_median,q_min))
 f_fq.close()
-f_out.close()
+#f_out.close()

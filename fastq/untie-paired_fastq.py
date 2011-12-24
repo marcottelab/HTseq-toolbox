f1af553e944ec6b6ae6abea11bfe58b9ce9dcd29
Taejoon Kwon <linusben@longhorn.tacc>
2011-12-24 08:27:00 -0600

    Add script to untie paired fastq file.

---
+++ fastq/untie-paired_fastq.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import os
+import sys
+import gzip
+
+filename_fq = sys.argv[1]
+
+filename_1 = filename_fq.replace('paired','untie_1')
+filename_2 = filename_fq.replace('paired','untie_2')
+
+if( filename_fq == filename_1 ):
+    sys.exit(1)
+
+f_fq = open(filename_fq,'r')
+f_1 = open(filename_1,'w')
+f_2 = open(filename_2,'w')
+
+for line in f_fq:
+    if( line.startswith('@') ):
+        h1 = line.strip()
+        nseq1 = f_fq.next().strip()
+        qh1 = f_fq.next()
+        qseq1 = f_fq.next().strip()
+    
+        h2 = f_fq.next().strip()
+        nseq2 = f_fq.next().strip()
+        qh2 = f_fq.next()
+        qseq2 = f_fq.next().strip()
+
+        f_1.write('%s\n%s\n+\n%s\n'%(h1,nseq1,qseq1))
+        f_2.write('%s\n%s\n+\n%s\n'%(h2,nseq2,qseq2))
+f_fq.close()
+f_1.close()
+f_2.close()
+

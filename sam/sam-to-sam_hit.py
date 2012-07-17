27ef78ac745a0c1fd560845fdd94c1a51ec83b5c
Taejoon Kwon <linusben@longhorn.tacc>
2012-07-17 03:42:32 -0500

    sam_hit/nohit filter.

--- sam/sam-to-sam_hit.py
+++ sam/sam-to-sam_hit.py
@@ -4,22 +4,29 @@ import sys
 import gzip
 
 filename_sam = sys.argv[1]
-filename_out = filename_sam+'_slim'
+
+filename_hit = filename_sam+'_hit'
+filename_nohit = filename_sam+'_nohit'
 
 f_sam = open(filename_sam,'r')
+
+f_hit = open(filename_hit,'w')
+f_nohit = open(filename_nohit,'w')
+
 if( filename_sam.endswith('.gz') ):
     f_sam = gzip.open(filename_sam,'rb')
     filename_out = filename_sam.replace('.gz','')+'_slim'
 
-f_out = open(filename_out,'w')
 for line in f_sam:
     if( line.startswith('@') ):
-        f_out.write('%s\n'%line.strip())
+        f_hit.write('%s\n'%line.strip())
         continue
 
     tokens = line.strip().split("\t")
     if( tokens[2] == '*' ):
+        f_nohit.write('@%s\n'%(tokens[0]))
         continue
-    f_out.write('%s\n'%"\t".join(tokens))
+    f_hit.write('%s\n'%line.strip())
 f_sam.close()
-f_out.close()
+f_hit.close()
+f_nohit.close()

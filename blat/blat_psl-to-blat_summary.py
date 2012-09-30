d3042919764f6629eda0459336a8ba0926672ce6
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2012-09-30 08:18:58 -0500

    Add strand info on final report

--- blat/blat_psl-to-blat_summary.py
+++ blat/blat_psl-to-blat_summary.py
@@ -45,7 +45,7 @@ for line in f_tbl:
         q2t[q_id][t_id] = dict()
         q2t_ratio[q_id][t_id] = 0
 
-    q2t[q_id][t_id] = {'q_size':q_size,'t_size':t_size,'block':','.join(block_list),'qstart':','.join(qstart_list),'tstart':','.join(tstart_list), 'q_gap_bases':q_gap_bases,'t_gap_bases':t_gap_bases,'q_gap_count':q_gap_count,'t_gap_count':t_gap_count}
+    q2t[q_id][t_id] = {'strand':strand,'q_size':q_size,'t_size':t_size,'block':','.join(block_list),'qstart':','.join(qstart_list),'tstart':','.join(tstart_list), 'q_gap_bases':q_gap_bases,'t_gap_bases':t_gap_bases,'q_gap_count':q_gap_count,'t_gap_count':t_gap_count}
     q2t_ratio[q_id][t_id] = match_ratio
 f_tbl.close()
 
@@ -58,6 +58,7 @@ for tmp_q in q2t_ratio.keys():
     q_gap_count1 = q2t[tmp_q][t1]['q_gap_count']  
     t_gap_bases1 = q2t[tmp_q][t1]['t_gap_bases']  
     t_gap_count1 = q2t[tmp_q][t1]['t_gap_count']  
+    strand1 = q2t[tmp_q][t1]['strand']
     block1 = q2t[tmp_q][t1]['block']
     qstart1 = q2t[tmp_q][t1]['qstart']
     tstart1 = q2t[tmp_q][t1]['tstart']
@@ -67,6 +68,7 @@ for tmp_q in q2t_ratio.keys():
     block2 = 'NA'
     qstart2 = 'NA'
     tstart2 = 'NA'
+    strand2 = 'NA'
     q_gap_bases2 = 0
     q_gap_count2 = 0
     t_gap_bases2 = 0
@@ -77,10 +79,11 @@ for tmp_q in q2t_ratio.keys():
         block2 = q2t[tmp_q][t2]['block']
         qstart2 = q2t[tmp_q][t2]['qstart']
         tstart2 = q2t[tmp_q][t2]['tstart']
+        strand2 = q2t[tmp_q][t1]['strand']
         q_gap_bases2 = q2t[tmp_q][t2]['q_gap_bases']  
         q_gap_count2 = q2t[tmp_q][t2]['q_gap_count']  
         t_gap_bases2 = q2t[tmp_q][t2]['t_gap_bases']  
         t_gap_count2 = q2t[tmp_q][t2]['t_gap_count']  
     
-    #print "%s\t%d\t%s\t%.3f\t%s\t%s\t%s\t%s\t%.3f\t%s\t%s\t%s"%(tmp_q,q_size,t1,ratio1,block1,qstart1,tstart1,t2,ratio2,block2,qstart2,tstart2)
-    print "%s\t%d\t%s\t%.3f\t%d\t%d\t%s\t%.3f\t%d\t%d"%(tmp_q,q_size,t1,ratio1,q_gap_bases1,q_gap_count1,t2,ratio2,q_gap_bases2,q_gap_count2)
+    print "%s\t%d\t%s\t%s\t%.3f\t%s\t%s\t%s\t%s\t%s\t%.3f\t%s\t%s\t%s"%(tmp_q,q_size,t1,straind1,ratio1,block1,qstart1,tstart1,t2,strand2,ratio2,block2,qstart2,tstart2)
+    #print "%s\t%d\t%s\t%.3f\t%d\t%d\t%s\t%.3f\t%d\t%d"%(tmp_q,q_size,t1,ratio1,q_gap_bases1,q_gap_count1,t2,ratio2,q_gap_bases2,q_gap_count2)

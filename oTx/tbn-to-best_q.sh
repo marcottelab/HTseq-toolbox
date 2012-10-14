bc78a096813ca81946dbde62442523f2c05f6b08
Taejoon Kwon <linusben@longhorn.tacc>
2012-10-13 22:36:48 -0500

    Add batch bash script for tbn-to-best_q

--- oTx/tbn-to-best_q.sh
+++ oTx/tbn-to-best_q.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-for TBL in $(ls *Vee*tbn+_tbl)
+for TBL in $(ls *tbn+_tbl)
 do
   OUT=${TBL/tbn+_tbl/best_q}
   echo $OUT
-  ./tbn-to-best_q.py $TBL > $OUT
+  ~/git/HTseq-toolbox/post/tbn-to-best_q.py $TBL > $OUT
 done
-
-#-rw-r--r-- 1 linusben G-25507 410M May  5 18:33 XENTR_ens66_pep.Amin201106_XENLA_nrC.tbn+_tbl
-#-rw-r--r-- 1 linusben G-25507 300M May  5 20:44 XENTR_ens66_pep.Chung201110_XENLA_nrC.tbn+_tbl
-#-rw-r--r-- 1 linusben G-25507 514M May  6 03:06 XENTR_ens66_pep.Park201106_XENLA_nrC.tbn+_tbl
-#-rw-r--r-- 1 linusben G-25507 442M May  6 07:43 XENTR_ens66_pep.Quigley201112_XENLA_nrC.tbn+_tbl
-#-rw-r--r-- 1 linusben G-25507 570M May 17 07:11 XENTR_ens66_pep.TXGP201107_XENLA_EGG_nrC.tbn+_tbl

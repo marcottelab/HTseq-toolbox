4b6a4d141c027a5a4996a204bed03a87e7bed90a
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-02-23 08:37:55 -0600

    change static home directory to $HOME

--- blast/tbl-to-best_bitscore.sh
+++ blast/tbl-to-best_bitscore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BEST="/home/00992/linusben/git/HTseq-toolbox/blast/tbl-to-best_bitscore.py"
+BEST="$HOME/git/HTseq-toolbox/blast/tbl-to-best_bitscore.py"
 for TBL in $(ls *_tbl)
 do
   OUT=$TBL"_best"

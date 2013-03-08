2cee664a8d80fbdea101ca7590785a1266b6471f
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-03-08 16:52:57 -0600

    Path correction on make-prot_final.sh

--- final/make-prot_final.sh
+++ final/make-prot_final.sh
@@ -3,5 +3,5 @@ for FA in $(ls *.prot_NR.fa)
 do
   PART=${FA/.fa/.self.bp+_tbl_part}
   echo "$FA, $PART"
-  ./make-prot_final.py $FA $PART
+  $HOME/git/HTseq-toolbox/final/make-prot_final.py $FA $PART
 done

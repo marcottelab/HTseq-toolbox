2d8a44cccba788b348fd7fd1a3013ee633158a92
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-08-05 10:30:46 -0500

    Scripts to detect 'holes' in genome mapping (designed for bacterial genome, not human)

---
+++ sam/plot-holes.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def read_holes(filename):
+    rv = dict()
+    f = open(filename,'r')
+    for line in f:
+        if( line.startswith('#') ):
+            continue
+        tokens = line.strip().split("\t")
+        target_id = tokens[0]
+        gap_start = int(tokens[1])
+        gap_len = int(tokens[3])
+        if( not rv.has_key(target_id) ):
+            rv[ target_id ] = dict()
+        rv[ target_id ][ gap_start ] = gap_len
+    f.close()
+    return rv
+
+sample_list = ['QB928','LC8','LC33','HR23','MR3','TR7A','TR7B']
+
+holes = dict()
+holes100 = dict()
+
+target_list = []
+for filename in os.listdir('.'):
+    if( filename.endswith('holes') ):
+        sample_name = filename.split('_')[1]
+        holes[sample_name] = read_holes(filename)
+        target_list += list(set(holes[sample_name].keys()))
+    elif( filename.endswith('holes100') ):
+        sample_name = filename.split('_')[1]
+        holes100[sample_name] = read_holes(filename)
+
+target_list = list(set(target_list))
+
+import matplotlib.pyplot as plt
+for target_id in target_list:
+    
+    fig = plt.figure(figsize=(12,5))
+    ax1 = fig.add_subplot(1,1,1)
+
+    holes_x = []
+    holes100_x = []
+    tmp_y = 0
+    for tmp_sample in sample_list:
+        if( holes100[tmp_sample].has_key(target_id) ):
+            for tmp_pos in holes100[tmp_sample][target_id]:
+                holes100_x.append( (tmp_pos, holes100[tmp_sample][target_id][tmp_pos]) )
+        if( holes[tmp_sample].has_key(target_id) ):
+            for tmp_pos in holes[tmp_sample][target_id]:
+                holes_x.append( (tmp_pos, holes[tmp_sample][target_id][tmp_pos]) )
+    
+        if( tmp_y == 0 ):
+            ax1.broken_barh(holes_x, (tmp_y, 0.8), facecolor='black', edgecolor='black')
+            ax1.broken_barh(holes100_x, (tmp_y, 0.8), facecolor='red', edgecolor='red')
+        else:
+            ax1.broken_barh(holes_x, (tmp_y, 0.8), facecolor='black', edgecolor='black')
+            ax1.broken_barh(holes100_x, (tmp_y, 0.8), facecolor='red', edgecolor='red')
+
+        tmp_y -= 1
+
+    ax1.set_xlim(0,4500000)
+    ax1.grid()
+    ax1.set_yticks([x+0.4 for x in range(0,tmp_y,-1)])
+    ax1.set_ylim(tmp_y+0.5,1)
+    ax1.set_yticklabels(sample_list)
+    ax1.set_title(target_id)
+    plt.savefig('%s.holes.png'%target_id.replace('|','_'))
+    plt.savefig('%s.holes.pdf'%target_id.replace('|','_'))
---
+++ sam/sam-to-holes.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import os
+import sys
+
+filename_sam = sys.argv[1]
+
+seq_map = dict()
+f_sam = open(filename_sam, 'r')
+for line in f_sam:
+    if( line.startswith('@') ):
+        if( line.startswith('@SQ') ):
+            tokens = line.strip().split()
+            seq_map[ tokens[1].split(':')[1] ] = [0 for x in range(0, int(tokens[2].split(':')[1]))]
+        continue
+
+    tokens = line.strip().split("\t")
+    t_id = tokens[2]
+    t_pos = int(tokens[3])
+    read_len = len(tokens[9])
+    if( t_id == '*' ):
+        continue
+
+    for tmp_i in range(0,read_len):
+        if( tmp_i+t_pos >= len(seq_map[t_id]) ):
+            continue
+        seq_map[t_id][tmp_i+t_pos] += 1
+f_sam.close()
+
+filename_base = filename_sam.replace('.sam','')
+
+f_holes = open('%s.holes'%filename_base,'w')
+f_holes100 = open('%s.holes100'%filename_base,'w')
+f_holes.write('#TargetID\tGapStart\tGapEnd\tGapLen\n')
+f_holes100.write('#TargetID\tGapStart\tGapEnd\tGapLen\n')
+
+for t_id in seq_map.keys():
+    start_list = []
+    end_list = []
+    for i in range(1,len(seq_map[t_id])):
+        if( seq_map[t_id][i-1] == 0 and seq_map[t_id][i] > 0 ):
+            end_list.append(i)
+        elif( seq_map[t_id][i-1] > 0 and seq_map[t_id][i] == 0 ):
+            start_list.append(i)
+
+    for i in range(1,len(start_list)):
+        tmp_diff = end_list[i+1] - start_list[i]
+        
+        f_holes.write("%s\t%d\t%d\t%d\n"%(t_id,start_list[i],end_list[i+1],tmp_diff))
+        if( tmp_diff > 100 ):
+            f_holes100.write("%s\t%d\t%d\t%d\n"%(t_id,start_list[i],end_list[i+1],tmp_diff))
+
+f_holes.close()
+f_holes100.close()

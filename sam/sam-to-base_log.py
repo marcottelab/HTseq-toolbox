3882a63fc7d769a4b231808888ef7ca6ebcae9e4
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2014-01-27 11:13:28 -0600

    Revised sam-to-base_log.py: bug fix, report basecalls

--- sam/sam-to-base_log.py
+++ sam/sam-to-base_log.py
@@ -4,38 +4,42 @@ import sys
 import gzip
 import re
 
-#filename_sam = sys.argv[1]
-#filename_base = re.sub(r'.sam[A-z_.]*$','',filename_sam)
 filename_base = sys.argv[1]
 
-min_mismatch = 20
+#min_mismatch = 20
+min_match = 20
 
-re_M = re.compile('\d+M')
-re_endM = re.compile('\d+M$')
-re_startM = re.compile('^\d+M')
-
-def get_startM_endM(tmp_cigar):
-    tmp_start = re_startM.search(tmp_cigar)
-    if( tmp_start == None ):
-        tmp_start = -1
-    else:
-        tmp_start = int(tmp_start.group(0).rstrip('M'))
-
-    tmp_end = re_endM.search(tmp_cigar)
-    if( tmp_end == None ):
-        tmp_end = -1
-    else:
-        tmp_end = int(tmp_end.group(0).rstrip('M'))
+## ref: http://sqt.googlecode.com/git-history/hashing/sqt/cigar.py
+def get_cigar(tmp_cigar):
+    rv = []
+    n = ''
+    count_M = 0
+    for c in tmp_cigar:
+        if c.isdigit():
+            n += c
+        else:
+            rv.append( (c, int(n)) )
+            if( c == 'M' ):
+                count_M += 1
+            n = ''
     
-    M_list = re_M.findall(tmp_cigar)
-    return tmp_start, tmp_end, sum([int(x.rstrip('M')) for x in M_list]), len(M_list)
+    matched_len = 0
+    start_pos = 0
+    if( count_M > 1 ):
+        return start_pos, matched_len
+    
+    for tag,align_len in rv:
+        if( tag == 'M' ):
+            matched_len = align_len
+            break
+        start_pos += align_len
+    
+    return start_pos, matched_len
 
-s_hits = dict()
-as_hits = dict()
 s_counts = dict()
 as_counts = dict()
-double_hits = dict()
-boundary_hits = dict()
+quad_counts = dict()
+boundary_counts = dict()
 
 A_counts = dict()
 T_counts = dict()
@@ -52,19 +56,21 @@ for filename_sam in os.listdir('.'):
     f_sam = open(filename_sam,'r')
     if( filename_sam.endswith('.gz') ):
         f_sam = gzip.open(filename_sam,'rb')
+    
+    seq_len = 10101
     for line in f_sam:
         if( line.startswith('@') and len(line.strip().split()[0]) < 4 ):
             if( line.startswith('@SQ') ):
                 tokens = line.strip().split()
                 seq_id = re.sub(r'SN:','',tokens[1])
                 seq_len = int(re.sub(r'LN:','',tokens[2]))
-                if( not s_hits.has_key(seq_id) ):
-                    s_hits[seq_id] = [0 for x in range(0,seq_len)]
-                    as_hits[seq_id] = [0 for x in range(0,seq_len)]
+                if( not s_counts.has_key(seq_id) ):
                     s_counts[seq_id] = [0 for x in range(0,seq_len)]
                     as_counts[seq_id] = [0 for x in range(0,seq_len)]
-                    double_hits[seq_id] = [0 for x in range(0,seq_len)]
-                    boundary_hits[seq_id] = [0 for x in range(0,seq_len)]
+                    s_counts[seq_id] = [0 for x in range(0,seq_len)]
+                    as_counts[seq_id] = [0 for x in range(0,seq_len)]
+                    quad_counts[seq_id] = [0 for x in range(0,seq_len)]
+                    boundary_counts[seq_id] = [0 for x in range(0,seq_len)]
                     A_counts[seq_id] = [0 for x in range(0,seq_len)]
                     T_counts[seq_id] = [0 for x in range(0,seq_len)]
                     G_counts[seq_id] = [0 for x in range(0,seq_len)]
@@ -83,90 +89,68 @@ for filename_sam in os.listdir('.'):
         if( hit_flag & 16 ):
             tmp_strand = '-'
 
-        tmp_startM, tmp_endM, tmp_sumM, tmp_countM = get_startM_endM(tmp_cigar)
-        tmp_mismatch_len = read_len - tmp_sumM
-        if( tmp_mismatch_len < min_mismatch ):
+        tmp_startM, tmp_matched_len = get_cigar(tmp_cigar)
+        if( tmp_matched_len < min_match ):
             continue
         
-        tmp_start = 0
-        tmp_end = 0
+        if( not s_counts.has_key(t_id) and not as_counts.has_key(t_id) ):
+            s_counts[t_id] = [0 for x in range(0,seq_len)]
+            as_counts[t_id] = [0 for x in range(0,seq_len)]
+            s_counts[t_id] = [0 for x in range(0,seq_len)]
+            as_counts[t_id] = [0 for x in range(0,seq_len)]
+            quad_counts[t_id] = [0 for x in range(0,seq_len)]
+            boundary_counts[t_id] = [0 for x in range(0,seq_len)]
+            A_counts[t_id] = [0 for x in range(0,seq_len)]
+            T_counts[t_id] = [0 for x in range(0,seq_len)]
+            G_counts[t_id] = [0 for x in range(0,seq_len)]
+            C_counts[t_id] = [0 for x in range(0,seq_len)]
+
+        tmp_start = start_pos + tmp_startM - 1
+        tmp_end = tmp_start + tmp_matched_len
         tmp_boundary_start = 0
         tmp_boundary_end = 0
-        ## aM case
-        if( tmp_startM > 0 and tmp_startM == tmp_endM ):
-            tmp_start = start_pos
-            tmp_end = start_pos + tmp_startM - 1
-        
-        ## aSbMcS case
-        if( tmp_endM < 0 and tmp_startM < 0 ):
-            if( tmp_countM == 1 ):
-                tmp_start = start_pos
-                tmp_end = start_pos + tmp_sumM - 1
+        if( tmp_matched_len < read_len ):
+            if( tmp_startM > 0 ):
                 tmp_boundary_start = tmp_start
+            if( tmp_startM + tmp_matched_len < read_len ):
                 tmp_boundary_end = tmp_end
 
-        ## aMbS case
-        elif( tmp_endM < 0 ):
-            tmp_start = start_pos
-            tmp_end = start_pos + tmp_startM - 1
-            tmp_boundary_end = tmp_end
-
-        ## aSbM case
-        elif( tmp_startM < 0 ):
-            tmp_start = start_pos
-            tmp_end = start_pos + tmp_endM - 1
-            tmp_boundary_start = tmp_start
-
-        #tmp_read_count = 1
-        tmp_read_count = int(read_id.split('_')[-1])
-        if( tmp_start > 0 and tmp_end > 0 ):
+        tmp_read_count = 1
+        #tmp_read_count = int(read_id.split('_')[-1])
+        for i in range(tmp_start, tmp_end+1):
             if( tmp_strand == '+' ):
-                for i in range(tmp_start-1, tmp_end):
-                    s_hits[t_id][i] += 1
-                    s_counts[t_id][i] += tmp_read_count
+                s_counts[t_id][i] += tmp_read_count
+            else:
+                as_counts[t_id][i] += tmp_read_count
 
-                    tmp_n = read_seq[i-start_pos+1]
-                    if( tmp_n == 'A' ):
-                        A_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'T' ):
-                        T_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'G' ):
-                        G_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'C' ):
-                        C_counts[t_id][i] += tmp_read_count
+            tmp_n = read_seq[i-start_pos]
+            if( tmp_n == 'A' ):
+                A_counts[t_id][i] += tmp_read_count
+            elif( tmp_n == 'T' ):
+                T_counts[t_id][i] += tmp_read_count
+            elif( tmp_n == 'G' ):
+                G_counts[t_id][i] += tmp_read_count
+            elif( tmp_n == 'C' ):
+                C_counts[t_id][i] += tmp_read_count
 
-                    if( i < tmp_end-1 ):
-                        double_hits[t_id][i] += 1
-            elif( tmp_strand == '-' ):
-                for i in range(tmp_start-1, tmp_end):
-                    as_hits[t_id][i] += 1
-                    as_counts[t_id][i] += tmp_read_count
-                    
-                    tmp_n = read_seq[i-start_pos+1]
-                    if( tmp_n == 'A' ):
-                        A_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'T' ):
-                        T_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'G' ):
-                        G_counts[t_id][i] += tmp_read_count
-                    elif( tmp_n == 'C' ):
-                        C_counts[t_id][i] += tmp_read_count
+            if( i == 990 and tmp_n != 'A' ):
+                print "+",read_id, start_pos, tmp_cigar, read_seq, tmp_start, tmp_end
 
-                    if( i < tmp_end-1 ):
-                        double_hits[t_id][i] += 1
+            if( i < tmp_end-3 ):
+                quad_counts[t_id][i] += 1
 
         if( tmp_boundary_start > 0 ):
-            boundary_hits[t_id][tmp_boundary_start-1] += 1
+            boundary_counts[t_id][tmp_boundary_start-1] += 1
         if( tmp_boundary_end > 0 ):
-            boundary_hits[t_id][tmp_boundary_end-1] += 1
+            boundary_counts[t_id][tmp_boundary_end-1] += 1
     f_sam.close()
     sys.stderr.write('Done\n')
 
-for t_id in s_hits.keys():
+for t_id in s_counts.keys():
     t_name = t_id.replace('|','_')
     filename_log = '%s.%s.base_log'%(filename_base,t_name)
     f_log = open(filename_log,'w')
-    f_log.write('Pos\tCount+\tCount-\tHit+\tHit-\tDouble\tBorder\tA\tT\tG\tC\n')
-    for i in range(0,len(s_hits[t_id])):
-        f_log.write("%d\t%d\t%d\t%d\t%d\t%d\t%d\n"%(i,s_counts[t_id][i], as_counts[t_id][i], s_hits[t_id][i],as_hits[t_id][i], double_hits[t_id][i], boundary_hits[t_id][i],A_counts[t_id][i],T_counts[t_id][i],G_counts[t_id][i],C_counts[t_id][i]))
+    f_log.write('Pos\tCount+\tCount-\tQuadruple\tBorder\tA\tT\tG\tC\n')
+    for i in range(0,len(s_counts[t_id])):
+        f_log.write("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n"%(i, s_counts[t_id][i], as_counts[t_id][i], quad_counts[t_id][i], boundary_counts[t_id][i],A_counts[t_id][i],T_counts[t_id][i],G_counts[t_id][i],C_counts[t_id][i]))
     f_log.close()

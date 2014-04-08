ac7271f3be044483007880a07b8746a7b28cd887
Taejoon Kwon <linusben@gmail.com>
2014-04-08 12:34:57 -0500

    Add argument for number of samples with cpm>1

--- exp/run-edgeR.R
+++ exp/run-edgeR.R
@@ -3,6 +3,7 @@ args <- commandArgs(trailingOnly = TRUE)
 filename_in <- args[1]
 sample1 <- args[2]
 sample2 <- args[3]
+min_cpm1 <- args[4]
 
 filename_base <- sub('.edgeR_in','',filename_in)
 filename_cpm <- sub('.edgeR_in','.edgeR_cpm',filename_in)
@@ -11,7 +12,7 @@ library(edgeR)
 
 x <- read.delim(filename_in,row.names='ID',header=T)
 groups <- gsub('_0[1234]$','',colnames(x))
-keep <- rowSums(cpm(x)>1) >= 3
+keep <- rowSums(cpm(x)>1) >= min_cpm1
 x <- x[keep,]
 
 y <- DGEList(counts=x,group=groups)
@@ -21,4 +22,4 @@ y <- estimateTagwiseDisp(y)
 
 write.table(cpm(x), file=filename_cpm, row.names=TRUE, col.names=TRUE, sep='\t')
 
-write.table(topTags(exactTest(y,pair=c(sample1,sample2)),n=Inf), paste(filename_base,paste(sample1,sample2,sep='-'),'edgeR_out',sep='.'),sep='\t')
+write.table(topTags(exactTest(y,pair=c(sample2,sample1)),n=Inf), paste(filename_base,paste(sample1,sample2,sep='-'),'edgeR_out',sep='.'),sep='\t')

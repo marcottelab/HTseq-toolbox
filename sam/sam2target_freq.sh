f4f704a08ea2ea69b877117ba95109e3560e5e82
Taejoon Kwon <linusben@gmail.com>
2014-11-18 11:52:24 -0600

    Clean up unused scripts for SAM files

--- sam/sam2target_freq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for SAM in $(ls *.sam)
-do
-  TARGET_FREQ=${SAM%.sam}".target_freq"
-  #TARGET_FREQ=$SAM".target_freq"
-  echo "$SAM --> $TARGET_FREQ"
-  gawk -F"\t" '{print $3"\t"$4"\t"$2"\t"$6}' $SAM | sort | uniq -c | sort -k 2,2 -k 3,3n > $TARGET_FREQ
-done

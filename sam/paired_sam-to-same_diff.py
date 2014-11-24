8101047e2e805fc61b5f741a6346d84f7d16134e
Taejoon Kwon <linusben@gmail.com>
2014-11-24 11:03:01 -0600

    Report unpaired and nohit also

--- sam/paired_sam-to-same_diff.py
+++ sam/paired_sam-to-same_diff.py
@@ -14,6 +14,8 @@ if( filename_sam.endswith('.gz') ):
 
 f_same = open('%s_same_t'%filename_base,'w')
 f_diff = open('%s_diff_t'%filename_base,'w')
+f_nohit = open('%s_nohit_t'%filename_base,'w')
+f_unpaired = open('%s_unpaired_t'%filename_base,'w')
 
 for line in f_sam:
     if( line.startswith('@') and len(line.split()[0]) < 4 ):
@@ -24,13 +26,21 @@ for line in f_sam:
     t1_id = tokens[2]
     t2_id = tokens[6]
     if( t1_id == '*' ):
+        f_nohit.write(line)
         continue
 
     if( t2_id == '=' ):
-        f_same.write(line)
+        t1_pos = int(tokens[3])
+        t2_pos = int(tokens[7])
+        if( t1_pos == t2_pos ):
+            f_unpaired.write(line)
+        else:
+            f_same.write(line)
     elif( t2_id != '*' ):
         f_diff.write(line)
 f_sam.close()
 
 f_same.close()
 f_diff.close()
+f_nohit.close()
+f_unpaired.close()
